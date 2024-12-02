ba2a87b075285d57f3a0ef86224e2b373495e5b9
joha0914 <jahongirqayumjonov26@gmail.com>
2024-12-02 10:41:21 +0500

    2 day

---
+++ 2. Red-Nosed Reports gold.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int const N = 1000;
+int a[N];
+map<int, int> m;
+int cnt;
+#define deb(x) cout << #x << " = " << x << '\n'
+vector<int> v;
+
+vector<int> make_vector(string input) {
+	vector<int> result;
+    stringstream ss(input);
+    int number;
+
+    while (ss >> number) {
+        result.push_back(number);
+    }
+
+    return result;
+}
+
+int solve(vector<int> org) {
+	vector<int> a = org, b = org;
+	
+	sort(a.begin(), a.end());
+	sort(b.rbegin(), b.rend());
+	
+	bool flg = true;
+	
+	for(int i = 0; i < org.size(); i ++) {
+		if (i > 0 && !(abs(org[i] - org[i - 1]) >= 1 && abs(org[i] - org[i - 1]) <= 3)) {
+			
+			flg = false;
+			break;
+		}
+		if(org[i] != a[i]) {
+			flg = false;
+			break;
+		}
+	}
+	
+	if (flg) return 1;
+	
+	flg = true;
+	for(int i = 0; i < org.size(); i ++) {
+		if (i > 0 && !(abs(org[i] - org[i - 1]) >= 1 && abs(org[i] - org[i - 1]) <= 3)) {
+			flg = false;
+			break;
+		}
+		
+		if(org[i] != b[i]) {
+			flg = false;
+			break;
+		}
+	}
+	
+	if (flg) return 1;
+	return 0;
+}
+int main () {
+	int t = N;
+	
+	while(t --> 0) {
+		string s;
+		getline(cin, s);
+		
+		vector<int> org = make_vector(s), helper;
+		
+		for(int i = 0; i < org.size() + 1; i ++) {
+			helper.clear();
+			for(int j = 0; j < org.size(); j ++) {
+				if(i != j) {
+					helper.push_back(org[j]);
+				}
+			}
+			
+			if(solve(helper)) {
+				cnt ++; break;
+			}
+		}
+	}
+	
+	cout << cnt;
+}
